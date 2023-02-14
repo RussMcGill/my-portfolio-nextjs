89fa3f4b29db10b9a7c2045db81374d367229c5a
Russ McGill <rmcgill@grastontechnique.com>
2023-02-14 16:52:26 -0500

    Update useTheme to work across all pages

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,14 @@
-import Head from 'next/head';
 import { Inter } from '@next/font/google';
-import Navbar from '@/components/Navbar';
 import MainInfo from '@/components/MainInfo';
 import Skills from '@/components/Skills';
 import About from '@/components/About';
-import { useEffect, useState } from 'react';
-import { useTheme } from 'next-themes';
+import Projects from '@/components/Projects';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  const { theme, setTheme } = useTheme();
-
-  useEffect(() => {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      setTheme('dark');
-    } else {
-      setTheme('light');
-    }
-  }, []);
-
-  useEffect(() => {
-    if (theme === 'dark') {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
-  }, [theme]);
-
   return (
     <>
-      <Head>
-        <title>Russ McGill | Software Engineer</title>
-        <meta name='description' content='Software Engineer' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Navbar theme={theme} setTheme={setTheme} />
       <div className='md:snap-y snap-mandatory h-screen w-screen overflow-auto'>
         <div className='snap-center md:h-screen w-screen'>
           <MainInfo />
@@ -46,6 +18,9 @@ export default function Home() {
         </div>
         <div className='snap-center md:h-screen w-screen'>
           <Skills />
+        </div>
+        <div className='snap-center md:h-screen w-screen'>
+          <Projects />
         </div>
       </div>
     </>
