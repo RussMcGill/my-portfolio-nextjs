2d45846bff664135ac6ecf714d48143e053ba948
Russ McGill <rmcgill@grastontechnique.com>
2023-02-14 16:53:04 -0500

    Initial projects push

---
+++ components/Project.jsx
@@ -0,0 +1,33 @@
+import { PROJECTS } from '@/lib/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Project({ title, backgroundImg, tech, projectUrl, techStack }) {
+  return (
+    <div className='relative flex flex-col items-center justify-between h-auto w-full bg-slate-300 dark:bg-slate-700 px-5 light:shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+      <div className='py-5'>
+        <h2 className='text-primary'>{title}</h2>
+      </div>
+      <div>
+        <img className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' />
+      </div>
+      <div className='hidden group-hover:block absolute w-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+        <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
+        <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
+        <div className='flex justify-center'>
+          <Link href={projectUrl}>
+            <button className=' flex-1 w-full text-center m-auto py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer px-5'>
+              More Info
+            </button>
+          </Link>
+        </div>
+      </div>
+      <div className='flex flex-wrap gap-2 justify-evenly m-2 group-hover:opacity-10'>
+        {techStack.map((tech) => {
+          return <div className=' bg-slate-600 dark:bg-slate-500 rounded p-1 text-white text-xs md:text-sm'>{tech}</div>;
+        })}
+      </div>
+    </div>
+  );
+}
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,7 +1,36 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import forestAdmin from '../public/assets/projects/forest_admin.png';
+import customPayment from '../public/assets/projects/custom-payments.png';
+import grastonApp from '../public/assets/projects/grastonApp.png';
+
+import Project from './Project';
+import { PROJECTS } from '@/lib/constants';
+
 export default function Projects() {
   return (
-    <div>
-      <h1>Projects</h1>
+    <div id='projects' className='w-full'>
+      <div className='max-w-[1240px] md:pt-28 mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
+        <h2 className='py-4'>What I&apos;ve Built</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
+          {PROJECTS.map((project) => {
+            return (
+              <Project
+                title={project.name}
+                backgroundImg={project.img}
+                projectUrl={project.url}
+                tech={project.mainTech}
+                techStack={project.techStack}
+              />
+            );
+          })}
+          {/* <Project title='CRM' backgroundImg={forestAdmin} projectUrl='/projects/crm' tech='Node JS' />
+          <Project title='Payment Calculator' backgroundImg={customPayment} projectUrl='/projects/payment_calculator' tech='Next JS' />
+          <Project title='Graston App' backgroundImg={grastonApp} projectUrl='/projects/graston-app' tech='Next JS' /> */}
+          {/* <Project title='Twitch UI' backgroundImg={twitchImg} projectUrl='/twitch' tech='Next JS' /> */}
+        </div>
+      </div>
     </div>
   );
 }
