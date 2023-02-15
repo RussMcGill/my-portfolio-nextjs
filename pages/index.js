4fb758b7fa9223864033792d0fdcfd51bc390a86
Russ McGill <rmcgill@grastontechnique.com>
2023-02-15 14:45:42 -0500

    Style changes

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,16 @@ export default function Home() {
   return (
     <>
       <div className='md:snap-y snap-mandatory h-screen w-screen overflow-auto'>
-        <div className='snap-center md:h-screen w-screen'>
+        <div className='snap-start md:h-screen w-screen'>
           <MainInfo />
         </div>
-        <div className='snap-center md:h-screen w-screen'>
+        <div className='snap-start md:h-screen w-screen'>
           <About />
         </div>
-        <div className='snap-center md:h-screen w-screen'>
+        <div className='snap-start md:h-screen w-screen'>
           <Skills />
         </div>
-        <div className='snap-center md:h-screen w-screen'>
+        <div className='snap-start md:h-screen w-screen'>
           <Projects />
         </div>
       </div>
