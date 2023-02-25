d1641c00bac9d39803781cfb7744399364369f56
Russ McGill <rmcgill@grastontechnique.com>
2023-02-25 18:56:20 -0500

    Update styling

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 import React from 'react';
 
-export default function Project({ title, backgroundImg, tech, projectUrl, techStack }) {
+export default function Project({ title, backgroundImg, projectUrl }) {
   return (
     <Link href={projectUrl}>
-      <div className="relative flex flex-col gap-5 items-center justify-between h-auto w-full bg-grey300 dark:bg-grey800 p-5 light:shadow-xl shadow-gray-400 rounded-xl">
+      <div className="relative flex flex-col gap-5 items-center justify-between h-auto w-full bg-white light:shadow-lg dark:bg-grey800 p-5 light:shadow-xl shadow-gray-400 rounded-xl">
         <div>
           <h2 className="text-primary dark:text-grey200">{title}</h2>
         </div>
