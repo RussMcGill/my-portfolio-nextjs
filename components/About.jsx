593b5f67cf0d0e54bfae39fcd6064f63d80e2fa2
Russ McGill <rmcgill@grastontechnique.com>
2023-02-25 19:04:45 -0500

    Style/responsiveness changes

--- components/About.jsx
+++ components/About.jsx
@@ -24,43 +24,53 @@ export default function About() {
   };
 
   return (
-    <div id='about' className='w-full md:h-screen p-2 flex items-center md:pt-24 pt-28'>
-      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
-        <motion.div variants={container} initial='hidden' whileInView='show' viewport={{ once: true }} className='col-span-2'>
-          <motion.p variants={pVariant} className='uppercase text-xl tracking-widest text-primary'>
-            About
-          </motion.p>
-          <motion.h2 variants={pVariant} className='md:py-4 py-2'>
-            Who I Am
-          </motion.h2>
-          <motion.p variants={pVariant} className='py-2 text-gray-600 dark:text-white'>
-            Short about title
-          </motion.p>
-          <motion.p variants={pVariant} className='py-2 text-gray-600 dark:text-white'>
-            I am Russ McGill, a highly motivated and dedicated professional with extensive experience in various industries. Throughout my career, I
-            have established myself as a leader in my field with a proven track record of success. My exceptional skills in problem-solving, strategic
-            planning, and relationship building have helped me make a significant impact in the organizations I have worked with.
-          </motion.p>
-          <motion.p variants={pVariant} className='py-2 text-gray-600 dark:text-white'>
-            In addition to my technical expertise, I am a gifted communicator and have built a reputation as a trusted advisor and reliable partner.
-            My interpersonal skills have helped me form strong relationships with clients, colleagues, and partners, and I bring a positive and
-            energetic approach to every project. I am always eager to take on new challenges and continue to develop my skills. With my combination of
-            technical knowledge, business acumen, and interpersonal skills, I am confident in my ability to make a valuable contribution to any
-            organization.
-          </motion.p>
-          <motion.div variants={pVariant}>
-            <Link href='/projects'> Check out some of my latest projects!</Link>
-          </motion.div>
-        </motion.div>
-        <motion.div
-          variants={dVariant}
-          initial='hidden'
-          whileInView='show'
+    <div id="about" className="w-full md:h-screen p-2 flex items-center md:pt-24 pt-28">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div
+          variants={container}
+          initial="hidden"
+          whileInView="show"
           viewport={{ once: true }}
-          className='w-full h-auto m-auto shadow-xl light:shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 z-0'
+          className="col-span-2"
         >
-          <img className='rounded-xl' src={'/assets/home/navy.jpg'} alt='' />
-        </motion.div>
+          <p variants={pVariant} className="uppercase text-xl tracking-widest text-primary">
+            About
+          </p>
+          <h2 variants={pVariant} className="md:py-4 py-2">
+            Who I Am
+          </h2>
+          <p variants={pVariant} className="py-2 text-gray-600 dark:text-white">
+            Short about title
+          </p>
+          <p variants={pVariant} className="py-2 text-gray-600 dark:text-white">
+            I am Russ McGill, a highly motivated and dedicated professional with extensive
+            experience in various industries. Throughout my career, I have established myself as a
+            leader in my field with a proven track record of success. My exceptional skills in
+            problem-solving, strategic planning, and relationship building have helped me make a
+            significant impact in the organizations I have worked with.
+          </p>
+          <p variants={pVariant} className="py-2 text-gray-600 dark:text-white">
+            In addition to my technical expertise, I am a gifted communicator and have built a
+            reputation as a trusted advisor and reliable partner. My interpersonal skills have
+            helped me form strong relationships with clients, colleagues, and partners, and I bring
+            a positive and energetic approach to every project. I am always eager to take on new
+            challenges and continue to develop my skills. With my combination of technical
+            knowledge, business acumen, and interpersonal skills, I am confident in my ability to
+            make a valuable contribution to any organization.
+          </p>
+          <div variants={pVariant}>
+            <Link href="/projects"> Check out some of my latest projects!</Link>
+          </div>
+        </div>
+        <div
+          // variants={dVariant}
+          // initial='hidden'
+          // whileInView='show'
+          // viewport={{ once: true }}
+          className="w-full h-auto m-auto shadow-xl light:shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 z-0"
+        >
+          <img className="rounded-xl" src={'/assets/home/navy.jpg'} alt="" />
+        </div>
       </div>
     </div>
   );
